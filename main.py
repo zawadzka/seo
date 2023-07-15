a9d1165912b5566982d840fbe00b6a4e5c710fd3
zawadzka <agnieszka.zawadzka@agencjawhites.pl>
2023-07-15 17:37:38 +0200

    streamlit set

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import streamlit as st
-# from google.oauth2 import service_account
-# from google.cloud.exceptions import NotFound
-# from google.cloud import bigquery
+import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
-# credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
-
-
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
 # secrets = toml.load('secrets.toml')["gcp_service_account"]
-# project_id = 'seo-project-392909'
-# secrets = st.secrets["gcp_service_account"]
+project_id = 'seo-project-392909'
+secrets = st.secrets["gcp_service_account"]
 # credentials = service_account.Credentials.from_service_account_info(secrets)
 # client = bigquery.Client(credentials=credentials)
-# client = bigquery.Client(credentials=credentials, project=project_id)
+client = bigquery.Client(credentials=credentials, project=project_id)
+bq_table = 'seo-project-392909.seo_dataset.data'
+#bq_secrets = secrets, bq_client = client,
+
+
+def bq_import(bq_table_name: str = bq_table):
+    s = """
+         SELECT time, sim_sum, text, size
+         FROM {} limit 10
+     """
+    sql = s.format(bq_table_name)
+    data = client.query(sql).to_dataframe()
+    return data
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
 
     st.write(f'Hi {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    data = bq_import()
+    st.table(data)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
