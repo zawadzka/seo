c97703479d83c788f90efdf46657b0aab2762220
zawadzka <agnieszka.zawadzka@agencjawhites.pl>
2023-07-16 01:07:44 +0200

    dataframe in streamlit

--- main.py
+++ main.py
@@ -54,16 +54,22 @@ def print_hi(name):
     st.dataframe(example_table)
 
     st.write('Search for site')
-    search_table = bq_search_query()
-    search_table['choice'] = pd.Series()#[0 for _ in range(len(search_table.index))])
-    search_table.loc[0, 'choice'] = 1
-    search_table = search_table.rename(columns={'sim_sum': 'similarity_keywords',
-                                                'full_content': 'content',
-                                                'pr': 'page_rank', 'time': 'response_time',
-                                                'size': 'file_size'})
-    search_table = search_table[['choice', 'name', 'content', 'similarity_keywords',
-                                 'page_rank', 'file_size', 'content_length', 'response_time',
-                                 'Number_of_Keywords']]
+    ch = st.checkbox('Do you want to perform a query?')
+    if ch:
+        search_table = bq_search_query()
+        search_table['choice'] = pd.Series()  # [0 for _ in range(len(search_table.index))])
+        search_table.loc[0, 'choice'] = 1
+        search_table.to_csv('static/search_table.csv')
+        search_table = search_table.rename(columns={'sim_sum': 'similarity_keywords',
+                                                    'full_content': 'content',
+                                                    'pr': 'page_rank', 'time': 'response_time',
+                                                    'size': 'file_size'})
+        search_table = search_table[['choice', 'name', 'content', 'similarity_keywords',
+                                     'page_rank', 'file_size', 'content_length', 'response_time',
+                                     'Number_of_Keywords']]
+    else:
+        search_table = pd.read_csv('static/search_table.csv')
+
     edited_df = st.data_editor(search_table,
                                column_config={
                                    'content': st.column_config.TextColumn(
