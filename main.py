2ca5086da0ab7cf4aca2df774e89d9e808754fce
zawadzka <agnieszka.zawadzka@agencjawhites.pl>
2023-07-16 10:32:31 +0200

    st.data_editor tests

--- main.py
+++ main.py
@@ -91,14 +91,17 @@ def main():
     # st.button('rerun')
     selected_indices = st.multiselect('Select rows:', search_table.name,
                                       default=search_table.name[0])
-    # content = search_table.loc[selected_indices[0], 'content']
-    # new_content = st.text_area('new content', content)
-    selected_indices
+    # selected_indices
     ind = search_table[search_table['name'] == selected_indices[0]].index[0]
     ind
+    content = search_table.loc[ind, 'content']
+    new_content = st.text_area('new content', content)
+    pr_v = search_table.loc[ind, 'pr']
+    new_pr = st.slider('page rank', 0.0, 1.0, pr_v, step=0.01)
+    st.write(new_pr, new_content)
     # try:
-        # ind = search_table[search_table['name'==selected_indices[0]].index[0]
-        #
+    # ind = search_table[search_table['name'==selected_indices[0]].index[0]
+    #
     #     pr_v = search_table.loc[ind, 'pr']
     #     content = edited_df.loc[ind[0][0], 'content']
     # except KeyError:
